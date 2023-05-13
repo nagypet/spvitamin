caee986472dddc55fe74359aa245cac34e4197a7
Peter Nagy <peter.nagy@perit.hu>
2023-05-13 14:49:51 +0200

    distribute-gradle-properties.bat

---
+++ distribute-gradle-properties.bat
@@ -0,0 +1,18 @@
+@echo off
+
+copy gradle.properties pwdtool
+copy gradle.properties spvitamin-core
+copy gradle.properties spvitamin-spring-admin
+copy gradle.properties spvitamin-spring-cloud-client
+copy gradle.properties spvitamin-spring-cloud-eureka
+copy gradle.properties spvitamin-spring-cloud-feign
+copy gradle.properties spvitamin-spring-data
+copy gradle.properties spvitamin-spring-general
+copy gradle.properties spvitamin-spring-logging
+copy gradle.properties spvitamin-spring-security
+copy gradle.properties spvitamin-spring-security-authservice
+copy gradle.properties spvitamin-spring-security-authservice-api
+copy gradle.properties spvitamin-spring-security-keycloak
+copy gradle.properties spvitamin-spring-security-ldap
+copy gradle.properties spvitamin-spring-server
+
